d3235637fb7847f91330f960ac14f8003854d735
evgzakharov <evgzakharov88@gmail.com>
2018-02-19 23:46:15 +0300

    refactoring jvm_server

---
+++ jvm_server/src/main/kotlin/ru/alfabank/ecomm/dcreator/server/DocumentGenerator.kt
@@ -0,0 +1,58 @@
+package ru.alfabank.ecomm.dcreator.server
+
+import ru.alfabank.ecomm.dcreator.parser.MarkdownParser
+import ru.alfabank.ecomm.dcreator.render.process.HeaderProcessor
+import ru.alfabank.ecomm.dcreator.render.serialize.FreemarkerRender
+import ru.alfabank.ecomm.dcreator.render.serialize.NodeSerializer
+import java.io.File
+
+class DocumentGenerator(
+        private val inputDirectory: File,
+        private val outputDirectory: File,
+        layoutDirectory: File
+) {
+    private val freemarkerRender = FreemarkerRender(layoutDirectory)
+
+    fun generateHtmlFromMd(generateFile: File) {
+        if (!generateFile.isFile)
+            return
+
+        if (generateFile.extension.toLowerCase() != "md") {
+            generateFile.copyTo(File(outputDirectory, generateFile.name), overwrite = true)
+            return
+        }
+
+        val relativePath = generateFile
+                .parentFile
+                .toRelativeString(inputDirectory).let {
+            if (it.isEmpty())
+                ""
+            else
+                "$it/"
+        }
+
+        val outputFile = File(outputDirectory, "$relativePath${generateFile.nameWithoutExtension}.html")
+
+        outputFile.parentFile.apply {
+            if (!exists()) mkdirs()
+        }
+
+        if (!outputFile.exists()) {
+            outputFile.createNewFile()
+        }
+
+        val node = MarkdownParser(inputDirectory).parse(generateFile)
+
+        val headerProcessor = HeaderProcessor()
+        val (result, replaceNodes) = headerProcessor.process(node)
+
+        val nodeSerializer = NodeSerializer(freemarkerRender, replaceNodes)
+
+        val preparedResult = result.mapValues { value ->
+            nodeSerializer.writeNodeToString(value.value)
+        }
+
+        val nodesData = freemarkerRender.render("index.ftlh", preparedResult)
+        outputFile.writeText(nodesData)
+    }
+}
---
+++ jvm_server/src/test/kotlin/ru/alfabank/ecomm/dcreator/server/ServerTest.kt
@@ -0,0 +1,2 @@
+package ru.alfabank.ecomm.dcreator.server
+
