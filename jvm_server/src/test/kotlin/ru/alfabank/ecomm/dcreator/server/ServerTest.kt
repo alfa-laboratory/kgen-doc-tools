e4f408de244a2d435ee4fdc036a5ba166a01d994
evgzakharov <evgzakharov88@gmail.com>
2018-02-20 00:25:40 +0300

    add simple server test (draft)

--- jvm_server/src/test/kotlin/ru/alfabank/ecomm/dcreator/server/ServerTest.kt
+++ jvm_server/src/test/kotlin/ru/alfabank/ecomm/dcreator/server/ServerTest.kt
@@ -1,2 +1,39 @@
 package ru.alfabank.ecomm.dcreator.server
 
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.net.ServerSocket
+
+class ServerTest {
+    val application = Application(
+            File("../files/input"),
+            File("../files/output/pages"),
+            File("../files/layout/freemarker"),
+            8081
+    )
+
+    @AfterEach
+    fun after() {
+        try {
+            application.stop()
+        } catch (e: Exception) {
+        }
+    }
+
+    @Test
+    fun `application test`() = runBlocking {
+        application.start(false)
+
+        val isConnected = try {
+            ServerSocket(8081)
+            false
+        } catch (e: Exception) {
+            true
+        }
+
+        assertTrue(isConnected)
+    }
+}
