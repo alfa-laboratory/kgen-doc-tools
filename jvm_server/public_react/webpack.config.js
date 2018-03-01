aeec245e47089024742032b453c908b210315542
evgzakharov <evgzakharov88@gmail.com>
2018-03-01 10:40:22 +0300

    change stub to more suitable

--- jvm_server/public_react/webpack.config.js
+++ jvm_server/public_react/webpack.config.js
@@ -1,70 +1,65 @@
-var path = require('path')
-var webpack = require('webpack')
+"use strict";
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "8888";
+
+loaders.push({
+  test: /\.scss$/,
+  loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
+  exclude: ['node_modules']
+});
 
 module.exports = {
-  entry: './src/main.js',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.jsx', // your app's entry point
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.s[a|c]ss$/,
-        loader: 'style!css!sass'
-      }
-    ]
+    publicPath: '/',
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['*', '.js', '.json']
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders
   },
   devServer: {
-    historyApiFallback: true,
+    contentBase: "./public",
+    // do not print bundle build stats
     noInfo: true,
-    overlay: true
+    // enable HMR
+    hot: true,
+    // embed the webpack-dev-server runtime into the bundle
+    inline: true,
+    // serve index.html in place of 404 responses to allow HTML5 history
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
   },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
+    }),
+    new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      files: {
+        css: ['style.css'],
+        js: [ "bundle.js"],
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+  ]
+};
