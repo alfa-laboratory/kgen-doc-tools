5cffce397c0df9271c3a99a54abd3394dcb0e366
evgzakharov <evgzakharov88@gmail.com>
2018-03-06 13:50:47 +0300

    draft with disabled all not finished modules

--- common_parser/src/test/kotlin/ru/alfabank/ecomm/dcreator/utils/Utils.kt
+++
@@ -1,8 +0,0 @@
-package ru.alfabank.ecomm.dcreator.utils
-
-import ru.alfabank.ecomm.dcreator.nodes.Node
-import ru.alfabank.ecomm.dcreator.nodes.TextBlockNode
-
-fun Node.toTextBlockNode(): TextBlockNode = TextBlockNode(listOf(this))
-
-fun List<Node>.toTextBlockNode(): TextBlockNode = TextBlockNode(this)
--- jvm_parser/src/main/kotlin/ru/alfabank/ecomm/dcreator/parser/common/UUID.kt
+++ jvm_parser/src/main/kotlin/ru/alfabank/ecomm/dcreator/parser/common/UUID.kt
@@ -5,6 +5,10 @@ import java.util.UUID as JUUID
 actual class UUID(private val uuid: JUUID) {
     actual fun print(): String = uuid.toString()
 
+    fun test() {
+        val test = ru.alfabank.ecomm.dcreator.nodes.CodeBlockNode("", "")
+    }
+
     companion object {
         actual fun randomUUID(): UUID  = UUID(JUUID.randomUUID())
     }
