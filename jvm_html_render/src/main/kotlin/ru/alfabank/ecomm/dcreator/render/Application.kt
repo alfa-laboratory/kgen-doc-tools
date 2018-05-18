9f87f06deb6f247ec6fe40bc4852bfebc7ea1b2d
evgzakharov <evgzakharov88@gmail.com>
2018-05-18 14:57:02 +0300

    revert some previous changes

--- jvm_html_render/src/main/kotlin/ru/alfabank/ecomm/dcreator/render/Application.kt
+++ jvm_html_render/src/main/kotlin/ru/alfabank/ecomm/dcreator/render/Application.kt
@@ -4,10 +4,10 @@ import kotlinx.coroutines.experimental.runBlocking
 import java.io.File
 
 fun main(args: Array<String>) = runBlocking {
-    val inputDirectory = File("filesExample/input")
-    val outputDirectory = File("filesExample/output/pages")
+    val inputDirectory = File("files/input")
+    val outputDirectory = File("files/output/pages")
 
-    val layoutPath = File("filesExample/layout")
+    val layoutPath = File("files/layout")
 
     val documentGenerator = DocumentGenerator(
         inputDirectory,
