33f6640ec21ed2cf6b3b83255cc3c0c309a2451e
evgzakharov <evgzakharov88@gmail.com>
2018-06-21 12:33:41 +0300

    fix test (in previous version IncludeLineParserTest instead of testing line parsing check block parser)

--- common_parser/src/test/kotlin/ru/alfabank/ecomm/dcreator/parser/line/IncludeLineParserTest.kt
+++ common_parser/src/test/kotlin/ru/alfabank/ecomm/dcreator/parser/line/IncludeLineParserTest.kt
@@ -1,11 +1,12 @@
 package ru.alfabank.ecomm.dcreator.parser.line
 
 import ru.alfabank.ecomm.dcreator.common.File
+import ru.alfabank.ecomm.dcreator.nodes.BoldNode
 import ru.alfabank.ecomm.dcreator.nodes.Node
+import ru.alfabank.ecomm.dcreator.nodes.RowLayout
 import ru.alfabank.ecomm.dcreator.nodes.TextNode
 import ru.alfabank.ecomm.dcreator.parser.MarkdownParser
 import ru.alfabank.ecomm.dcreator.test.runTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,17 +14,19 @@ class IncludeLineParserTest {
     private val parser = MarkdownParser(File("../common_parser/src/test/resources/includeLineTest"))
     private val lineParser = parser.lineParser
 
-    @BeforeTest
-    fun before() {
-        parser.lineParsers.clear()
-        parser.lineParsers.add(IncludeLineParser(parser))
-    }
-
     @Test
     fun test_line_include() = runTest {
-        val parseResult: Node = lineParser.parse("::include \"test.md\"")
-        val expectedResult = TextNode("some text from test.md")
+        val parseResult: Node = lineParser.parse("**some text** ::include \"test.md\" **some after text**")
+        val expectedResult = RowLayout(
+            nodes = listOf(
+                BoldNode(node = TextNode(text = "some text")),
+                TextNode(text = " some text from test.md "),
+                BoldNode(node = TextNode(text = "some after text"))
+            )
+        )
 
-        assertEquals(expectedResult, parseResult)
+        assertEquals(
+            expectedResult, parseResult
+        )
     }
 }
